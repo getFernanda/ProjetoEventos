506e7ad69b1efc723950acbbf0ac0dcbce105627
Pedro Viana Araújo Silva <pedrovianaaraujosilva@gmail.com>
2020-12-28 16:36:17 -0300

    adicionando a rota pra página de cadastros

--- weGether/routes/index.js
+++ weGether/routes/index.js
@@ -6,4 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/cadastro', function(req, res, next){
+  res.render('cadastro')
+})
 module.exports = router;
