33a970c9d01bbc8c1418dcad7737634ca65bf492
getFernanda <fernandapersil@gmail.com>
2021-01-03 18:34:22 -0300

    adicionando pagina de seguranca.ejs, home_do_evento.ejs, corrigindo rotas e includes login, dados pessoais e meus eventos

--- weGether/routes/index.js
+++ weGether/routes/index.js
@@ -13,4 +13,29 @@ router.get('/cadastro', function(req, res, next){
 router.get('/plenaria', function(req, res, next){
   res.render('plenaria')
 });
+
+router.get('/seguranca', function(req, res, next){
+  res.render('seguranca')
+});
+
+router.get('/seguranca', function(req, res, next){
+  res.render('seguranca')
+});
+
+router.get('/home-do-evento', function(req, res, next){
+  res.render('home_do_evento')
+});
+
+router.get('/dados-pessoais', function(req, res, next) {
+  res.render('dados_pessoais');
+});
+
+router.get('/meus-eventos', function(req, res, next) {
+  res.render('meus_eventos');
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
 module.exports = router;
