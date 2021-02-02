6893453924a15afcbeb008c0f3bfd877b6a492ca
mcsm01 <mayramedeiros83@gmail.com>
2021-02-02 18:08:29 -0300

    atualizando o footer, cabeçalho e mobile da pagina segurança

--- weGether/app.js
+++ weGether/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var authRouter = require('./routes/authentication');
+//var eventoRouter = require('./routes/evento');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/auth', authRouter);
+app.use('/evento',eventoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
