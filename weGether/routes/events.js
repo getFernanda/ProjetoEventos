22ed8a5940adaa7885c274520712c71b883df2eb
Pedro Viana Araújo Silva <pedrovianaaraujosilva@gmail.com>
2021-02-12 22:44:27 -0300

    criando rota pra página de eventos

--- weGether/routes/events.js
+++ weGether/routes/events.js
@@ -1,9 +1,9 @@
 var express = require('express');
+const EventController = require('../controllers/EventController');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/create', function(req, res, next) {
-    res.render('registrar-evento');
-});
+router.get('/create', EventController.showCreateEventForm);
+router.post('/create', EventController.createEvent);
 
 module.exports = router;
